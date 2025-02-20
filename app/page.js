fae01df7de18d73f3dbbbe9c2ffc10a74c550c26
jyhyun1008 <jayhyun1008@gmail.com>
2025-02-20 16:37:47 +0900

    remove workflow

--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
 'use client'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Home() {
 
   const [url, setUrl] = useState('')
 
+  useEffect(()=>{
+    if (localStorage.getItem('calendarUrl') && localStorage.getItem('calendarUrl') != '') {
+      setUrl(localStorage.getItem('calendarUrl'))
+      location.href = '/'+localStorage.getItem('calendarUrl')
+    }
+  }, [])
+
   function changeUrl(e) {
     setUrl(encodeURIComponent(e.target.value))
+    localStorage.setItem('calendarUrl', encodeURIComponent(e.target.value))
   }
 
   return (
